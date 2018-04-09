a60ec89dd9a348cecdcd49a0152279440cc6a66a
Neil Booth <kyuupichan@gmail.com>
2018-04-09 13:57:03 +0900

    Release 0.5.5

--- aiorpcx/__init__.py
+++ aiorpcx/__init__.py
@@ -5,7 +5,7 @@ from .socks import *
 from .session import *
 from .util import *
 
-_version = (0, 5, 4)
+_version = (0, 5, 5)
 _version_str = '.'.join(str(part) for part in _version)
 
 __all__ = (framing.__all__ +
