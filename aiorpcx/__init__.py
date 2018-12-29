d6342b79e9cdb3195458ff6a3aada507706ef673
Neil Booth <kyuupichan@gmail.com>
2018-12-29 17:32:37 +0800

    setup.py: read the file to extract the version
    Closes #10

--- aiorpcx/__init__.py
+++ aiorpcx/__init__.py
@@ -5,8 +5,10 @@ from .socks import *
 from .session import *
 from .util import *
 
-_version = (0, 10, 1)
-_version_str = '.'.join(str(part) for part in _version)
+
+_version_str = '0.10.2'
+_version = tuple(int(part) for part in _version_str.split('.'))
+
 
 __all__ = (curio.__all__ +
            framing.__all__ +
--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
+import os.path
+import re
 import setuptools
 
-from aiorpcx import _version_str as version
+
+def find_version(filename):
+    with open(filename) as f:
+        text = f.read()
+    match = re.search(r"^_version_str = '(.*)'$", text, re.MULTILINE)
+    if not match:
+        raise RuntimeError('cannot find version')
+    return match.group(1)
+
+
+tld = os.path.abspath(os.path.dirname(__file__))
+version = find_version(os.path.join(tld, 'aiorpcx', '__init__.py'))
 
 
 setuptools.setup(
